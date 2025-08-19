835926711d5c0f8d9277aac2df000d1d93118ec7
SERES\juan.medina <juan.medina@seres.es>
2025-08-19 10:31:37 +0200

    revert

--- src/galerias/entities/galeria.entity.ts
+++ src/galerias/entities/galeria.entity.ts
@@ -1,29 +1,24 @@
-// src/galerias/entities/galeria.entity.ts
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Artista } from '../../artistas/entities/artista.entity';
 import { Exposicion } from '../../exposiciones/entities/exposicion.entity';
+import { Artista } from '../../artistas/entities/artista.entity';
 
-@ObjectType()
 @Entity()
 export class Galeria {
-  @Field( () => Int )
   @PrimaryGeneratedColumn()
   id_galeria: number;
 
-  @Field()
   @Column()
   nombre: string;
 
-  @Field( { nullable: true } )
   @Column( { nullable: true } )
-  direccion?: string;
+  email?: string;
 
-  @Field( () => [Artista], { nullable: true } )
-  @OneToMany( () => Artista, artista => artista.galeria )
-  artistas?: Artista[];
+  @Column( { nullable: true } )
+  telefono?: string;
 
-  @Field( () => [Exposicion], { nullable: true } )
   @OneToMany( () => Exposicion, exposicion => exposicion.galeria )
-  exposiciones?: Exposicion[];
+  exposiciones: Exposicion[];
+
+  @OneToMany( () => Artista, artista => artista.galeria )
+  artistas: Artista[];
 }
