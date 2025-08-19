f30012782fc1059b9a9d19c9cb9951214808f5e6
SERES\juan.medina <juan.medina@seres.es>
2025-08-19 08:07:48 +0200

    Fix auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 // src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthResolver } from './auth.resolver';
+import { Usuario } from '../usuarios/entities/usuario.entity';
 
-@Module({
+@Module( {
+  imports: [
+    TypeOrmModule.forFeature( [Usuario] ), // 👈 para poder inyectar Repository<Usuario>
+    JwtModule.register( {
+      secret: process.env.JWT_SECRET || 'super-secret', // ⚠️ ponlo en .env
+      signOptions: { expiresIn: '1d' },
+    } ),
+  ],
   providers: [AuthService, JwtStrategy, AuthResolver],
-  exports: [AuthService], // exporta AuthService si otros módulos lo necesitan
-})
+  exports: [AuthService],
+} )
 export class AuthModule { }
