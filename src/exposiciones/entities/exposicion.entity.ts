ef99ab12eddcd0aaca7277c0ee4cd037225fcaa3
SERES\juan.medina <juan.medina@seres.es>
2025-08-19 10:26:57 +0200

    Decorated

--- src/exposiciones/entities/exposicion.entity.ts
+++ src/exposiciones/entities/exposicion.entity.ts
@@ -1,25 +1,34 @@
+// src/exposiciones/entities/exposicion.entity.ts
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Galeria } from '../../galerias/entities/galeria.entity';
 import { Obra } from '../../obras/entities/obra.entity';
 import { Artista } from '../../artistas/entities/artista.entity';
 
+@ObjectType()
 @Entity()
 export class Exposicion {
+  @Field( () => Int )
   @PrimaryGeneratedColumn()
   id_exposicion: number;
 
+  @Field()
   @Column()
   titulo: string;
 
-  @Column({ nullable: true })
+  @Field( { nullable: true } )
+  @Column( { nullable: true } )
   descripcion?: string;
 
-  @ManyToOne(() => Galeria, galeria => galeria.exposiciones)
+  @Field( () => Galeria )
+  @ManyToOne( () => Galeria, galeria => galeria.exposiciones )
   galeria: Galeria;
 
-  @ManyToOne(() => Artista, artista => artista.exposiciones, { nullable: true })
+  @Field( () => Artista, { nullable: true } )
+  @ManyToOne( () => Artista, artista => artista.exposiciones, { nullable: true } )
   artista?: Artista;
 
-  @OneToMany(() => Obra, obra => obra.exposicion)
-  obras: Obra[];
+  @Field( () => [Obra], { nullable: true } )
+  @OneToMany( () => Obra, obra => obra.exposicion )
+  obras?: Obra[];
 }
