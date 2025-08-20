b2e9f115e31d94a209488af0558573b14ecd3cfa
SERES\juan.medina <juan.medina@seres.es>
2025-08-20 17:14:46 +0200

    avoid cors

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap () {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create( AppModule );
+  app.useGlobalPipes( new ValidationPipe() );
+
+  app.enableCors( {
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  } );
 
   // Usar el puerto asignado por Railway, o 3000 como fallback local
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen( port );
 
-  console.log(`Server running on port ${port}`);
+  console.log( `Server running on port ${port}` );
 }
 bootstrap();
