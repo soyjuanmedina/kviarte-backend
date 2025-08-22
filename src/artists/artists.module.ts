69c0f2eaa1b3e2cfb5783a1ea3758180b898654a
SERES\juan.medina <juan.medina@seres.es>
2025-08-22 19:41:00 +0200

    Add module

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,8 +1,14 @@
 // src/artistas/artistas.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Artist } from './entities/artist.entity';
+import { Galeria } from '../galerias/entities/galeria.entity';
+import { ArtistsService } from './artists.service';
+import { ArtistsResolver } from './artists.resolver';
 
 @Module( {
-  providers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature( [Artist, Galeria] )],
+  providers: [ArtistsService, ArtistsResolver],
+  exports: [ArtistsService],
 } )
 export class ArtistsModule { }
