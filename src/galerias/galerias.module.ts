9939b2869de81fce1eea3111bf30051a506feb8f
SERES\juan.medina <juan.medina@seres.es>
2025-08-22 16:14:14 +0200

    New galery module

--- src/galerias/galerias.module.ts
+++ src/galerias/galerias.module.ts
@@ -1,8 +1,13 @@
 // src/galerias/galerias.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GaleriasService } from './galerias.service';
+import { GaleriasResolver } from './galerias.resolver';
+import { Galeria } from './entities/galeria.entity';
 
-@Module({
-  providers: [],
-  exports: [],
-})
+@Module( {
+  imports: [TypeOrmModule.forFeature( [Galeria] )],
+  providers: [GaleriasService, GaleriasResolver],
+  exports: [GaleriasService],
+} )
 export class GaleriasModule { }
