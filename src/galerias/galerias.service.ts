0d867b3442cfa4b415ff64eae6352c5485ebb551
SERES\juan.medina <juan.medina@seres.es>
2025-08-24 18:57:28 +0200

    Remove consoles

--- src/galerias/galerias.service.ts
+++ src/galerias/galerias.service.ts
@@ -29,17 +29,8 @@ export class GaleriasService {
   async update ( id: number, data: Partial<Galeria> ): Promise<Galeria> {
     const galeria = await this.repo.findOne( { where: { id_galeria: id } } );
     if ( !galeria ) throw new NotFoundException( `Galería con id ${id} no encontrada` );
-
-    console.log( 'Antes de asignar:', galeria );
-    console.log( 'Datos a actualizar:', data );
-
     Object.assign( galeria, data );
-
-    console.log( 'Después de asignar:', galeria );
-
     const saved = await this.repo.save( galeria );
-    console.log( 'Guardado:', saved );
-
     return saved;
   }
 
