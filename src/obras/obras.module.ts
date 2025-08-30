a1790e837b5295c09b471efe6117ead9e4ee2007
SERES\juan.medina <juan.medina@seres.es>
2025-08-30 18:30:49 +0200

    obras module

--- src/obras/obras.module.ts
+++ src/obras/obras.module.ts
@@ -1,8 +1,15 @@
 // src/obras/obras.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Obra } from './entities/obra.entity';
+import { Artist } from '../artists/entities/artist.entity';
+import { Exposicion } from '../exposiciones/entities/exposicion.entity';
+import { ObrasService } from './obras.service';
+import { ObrasResolver } from './obras.resolver';
 
-@Module({
-  providers: [],
-  exports: [],
-})
+@Module( {
+  imports: [TypeOrmModule.forFeature( [Obra, Artist, Exposicion] )],
+  providers: [ObrasService, ObrasResolver],
+  exports: [ObrasService],
+} )
 export class ObrasModule { }
