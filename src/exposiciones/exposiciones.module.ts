f287bca416249294ffdd1f481d0b84b673bdff5e
SERES\juan.medina <juan.medina@seres.es>
2025-09-03 15:32:17 +0200

    import

--- src/exposiciones/exposiciones.module.ts
+++ src/exposiciones/exposiciones.module.ts
@@ -5,9 +5,10 @@ import { ExposicionesResolver } from './exposiciones.resolver';
 import { Exposicion } from './entities/exposicion.entity';
 import { Galeria } from '../galerias/entities/galeria.entity';
 import { Artist } from '../artists/entities/artist.entity';
+import { ObrasModule } from '../obras/obras.module';
 
 @Module( {
-  imports: [TypeOrmModule.forFeature( [Exposicion, Galeria, Artist] )],
+  imports: [TypeOrmModule.forFeature( [Exposicion, Galeria, Artist] ), ObrasModule],
   providers: [ExposicionesService, ExposicionesResolver],
 } )
 export class ExposicionesModule { }
