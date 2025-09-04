5f75d5227655b2ad25621b87f806b2936bc08cce
SERES\juan.medina <juan.medina@seres.es>
2025-09-04 18:05:08 +0200

    Cambio en promotion

--- src/promotions/entities/promotion.entity.ts
+++ src/promotions/entities/promotion.entity.ts
@@ -6,38 +6,30 @@ import { Obra } from '../../obras/entities/obra.entity';
 @ObjectType()
 @Entity( 'promotions' )
 export class Promotion {
-  @Field( () => ID )
-  @PrimaryGeneratedColumn( { name: 'id_oferta' } ) // en tu BBDD es id_oferta
+  @Field( () => ID, { name: 'id_promotion' } )
+  @PrimaryGeneratedColumn( { name: 'id_oferta' } )
   id: number;
 
-  @Field()
-  @Column( { unique: true } )
-  code: string;
+  @Field( () => Float, { name: 'precio' } )
+  @Column( 'decimal' )
+  discount: number;
 
   @Field( { nullable: true } )
   @Column( { nullable: true } )
   description?: string;
 
-  @Field( () => Float )
-  @Column( 'decimal' )
-  discount: number;
-
-  @Field()
-  @Column( { name: 'fecha_inicio', type: 'date' } )
-  startDate: Date;
-
-  @Field()
-  @Column( { name: 'fecha_fin', type: 'date' } )
-  endDate: Date;
+  @Field( { defaultValue: true } )
+  @Column( { default: true } )
+  activa: boolean;
 
   @Field( () => Galeria )
   @ManyToOne( () => Galeria, galeria => galeria.promotions, { onDelete: 'CASCADE' } )
   galeria: Galeria;
 
-  @Field( () => [Obra], { nullable: true } )
+  @Field( () => [Obra], { nullable: true, name: 'obra' } )
   @ManyToMany( () => Obra, obra => obra.promotions, { cascade: true } )
   @JoinTable( {
-    name: 'ofertas_obras', // tu tabla intermedia
+    name: 'ofertas_obras',
     joinColumn: { name: 'id_oferta', referencedColumnName: 'id' },
     inverseJoinColumn: { name: 'id_obra', referencedColumnName: 'id_obra' }
   } )
